7bf5b26afdbbc3de2344cd71da2ffdc00adc0353
Ismarji Workman <ismarjiw@protonmail.com>
2024-02-11 22:03:00 -0500

    experiment with process

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import * as emailjs from 'emailjs-com';
 import { EmailJSResponseStatus } from 'emailjs-com';
+import { environment } from '../../environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
   
-  emailJsApiKey: string = process.env['EMAIL_JS_API_KEY']!; 
+  // emailJsApiKey: string = process.env['EMAIL_JS_API_KEY']!; 
+  emailJsApiKey = environment.emailJsApiKey;
 
   constructor() { }
 
